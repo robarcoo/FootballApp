9ad33028e36c531b5aeef1ebb25c35e21ed7fd06
Nastya051 <ladinskaya051@yandex.ru>
2024-04-08 12:46:42 +0300

    Rename pages to screens. Delete extra 'Row's. Change LocalContext.current to stringResource().

--- app/src/main/java/com/example/footballplayassistant/presentation/MainActivity.kt
+++ app/src/main/java/com/example/footballplayassistant/presentation/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.footballplayassistant.presentation
 
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
-import com.example.footballplayassistant.presentation.pages.SignInPage
-import com.example.footballplayassistant.presentation.pages.StartPage
+import com.example.footballplayassistant.presentation.screens.StartPage
 import com.example.footballplayassistant.ui.theme.FootballPlayAssistantTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,21 +27,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FootballPlayAssistantTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/footballplayassistant/presentation/customviews/ForgotPassword.kt
+++ app/src/main/java/com/example/footballplayassistant/presentation/customviews/ForgotPassword.kt
@@ -9,17 +9,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.footballplayassistant.R
 
 @Composable
 @Preview
-fun ForgotPassword(){
-    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+fun ForgotPassword(modifier: Modifier = Modifier){
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
         Button(colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.Black),
             onClick = { /*TODO*/ }) {
-            Text(text = "Забыли пароль?", fontSize = 12.sp, fontWeight = FontWeight.W500)
+            Text(text = stringResource(id = R.string.forgotPassword), fontSize = 12.sp, fontWeight = FontWeight.W500)
         }
     }
 }
