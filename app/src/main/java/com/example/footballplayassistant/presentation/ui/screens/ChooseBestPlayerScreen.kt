68bb195de80841c71eace2a6da37a929cb05007f
Nastya051 <ladinskaya051@yandex.ru>
2024-04-16 15:04:59 +0300

    Added ChooseBestPlayerScreen.

---
+++ app/src/main/java/com/example/footballplayassistant/presentation/ui/screens/ChooseBestPlayerScreen.kt
@@ -0,0 +1,10 @@
+package com.example.footballplayassistant.presentation.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun ChooseBestPlayerScreen(){
+
+}
