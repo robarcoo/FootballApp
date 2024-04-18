7d1c0be4c731033bdf2c13a89638d1b5e7be32f2
andrewlis <andrewlis4774@gmail.com>
2024-04-18 10:26:33 +0300

    fixed color bugs

--- app/src/main/java/com/example/footballplayassistant/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/footballplayassistant/presentation/ui/theme/Theme.kt
@@ -1,17 +1,13 @@
 package com.example.footballplayassistant.presentation.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -58,7 +54,8 @@ fun FootballPlayAssistantTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
+    // Убираем разные цвета в зависимости от темы, потому что пока нужна только одна тема
+    /*val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
@@ -66,7 +63,8 @@ fun FootballPlayAssistantTheme(
 
         darkTheme -> LightColorScheme
         else -> LightColorScheme
-    }
+    }*/
+    val colorScheme = LightColorScheme
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
