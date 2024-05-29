96eaf35b78cb38c801ce6ab8d67fa1ba0995a254
Nastya051 <ladinskaya051@yandex.ru>
2024-05-29 18:43:08 +0300

    Added SubscriptionsScreen, SubscriberRow and FilterSubscribers enum.

--- app/src/main/java/com/example/footballplayassistant/presentation/navigation/Route.kt
+++ app/src/main/java/com/example/footballplayassistant/presentation/navigation/Route.kt
@@ -64,6 +64,8 @@ sealed class Route(val path: String) {
 
     data object SafetyScreen : Route(path = "safety_screen")
 
+    data object SubscriptionsScreen : Route(path = "subscriptions_screen")
+
 
 
     fun withArgs(vararg args: String): String {
